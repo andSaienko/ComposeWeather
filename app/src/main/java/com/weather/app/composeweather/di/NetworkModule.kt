b1be5fd1e6b1a8262ced1bb6c9a65ee170a96d48
Andrii Isaienko <ivan.pletinskij@tabby.ai>
2023-11-29 19:03:19 +0200

    v1.1 (small architecture fixes)

--- app/src/main/java/com/weather/app/composeweather/di/NetworkModule.kt
+++ app/src/main/java/com/weather/app/composeweather/di/NetworkModule.kt
@@ -1,24 +1,46 @@
 package com.weather.app.composeweather.di
 
-import com.weather.app.composeweather.data.api.OkHttpClientImpl
-import com.weather.app.composeweather.data.api.RetrofitProvider
-import com.weather.app.composeweather.data.api.WeatherApiServiceProvider
 import com.weather.app.composeweather.data.api.WeatherApi
+import com.weather.app.composeweather.data.api.WeatherApiServiceProvider
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
 
     factory<OkHttpClient> {
-        OkHttpClientImpl().provideOkHttpClient()
+        provideOkHttpClient()
     }
 
     factory<Retrofit> {
-        RetrofitProvider(okHttpClient = get()).provideRetrofit()
+        provideRetrofit(okHttpClient = get())
     }
 
     factory<WeatherApi> {
         WeatherApiServiceProvider().provideWeatherApiService(retrofit = get())
     }
+}
+
+fun provideOkHttpClient(): OkHttpClient {
+    val createAuthInterceptor = Interceptor { chain ->
+        chain.proceed(chain.request().newBuilder().build())
+    }
+
+    return OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .addInterceptor(createAuthInterceptor)
+        .build()
+}
+
+fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+    return Retrofit.Builder()
+        .baseUrl("https://api.weatherapi.com")
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 }
